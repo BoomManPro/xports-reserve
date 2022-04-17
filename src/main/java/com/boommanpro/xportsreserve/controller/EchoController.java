73a7984c884e8189d600cab8333979bb584e0819
BoomMan <l.o.v.e_y.o.u_3344@qq.com>
2022-04-17 12:54:57 +0800

    feat(remove not use controller):

--- src/main/java/com/boommanpro/xportsreserve/controller/EchoController.java
+++ src/main/java/com/boommanpro/xportsreserve/controller/EchoController.java
@@ -1,10 +1,6 @@
 package com.boommanpro.xportsreserve.controller;
 
-import com.boommanpro.xportsreserve.config.AccountInfo;
-import com.boommanpro.xportsreserve.config.VenuePageConfigProperties;
 import com.boommanpro.xportsreserve.model.ResultVo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,17 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("echo")
 public class EchoController {
 
-    @Autowired
-    private AccountInfo accountInfo;
-
-    @Autowired
-    private VenuePageConfigProperties venuePageConfigProperties;
-
-    @Autowired
-    private Environment environment;
-
     @GetMapping
-    public ResultVo<AccountInfo> echo() {
-        return ResultVo.success(accountInfo);
+    public ResultVo<String> alive(){
+        return ResultVo.success("alive");
     }
 }
